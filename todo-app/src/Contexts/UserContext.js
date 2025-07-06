aa9bbef9e2c534514a2526713e7471422b2b459f
neeraj3bhardwaj <bhardwajneeraj.2005@gmail.com>
2025-07-06 13:05:01 +0530

    Delete todo-app directory

--- todo-app/src/Contexts/UserContext.js
+++
@@ -1,9 +0,0 @@
-import {useContext, createContext} from "react"
-
-export const userContext = createContext({})
-
-export const userProvider = userContext.Provider
-
-export default function useUserContext () {
-    return useContext(userContext)
-}
